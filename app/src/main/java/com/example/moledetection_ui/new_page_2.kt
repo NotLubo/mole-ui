a1b43b5019ba18e711edf08a93df8b8a5ff1cacc
luborepka@gmail.com <236G896h>
2020-11-20 18:49:09 +0100

    init

---
+++ app/src/main/java/com/example/moledetection_ui/new_page_2.kt
@@ -0,0 +1,11 @@
+package com.example.moledetection_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class new_page_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_page_2)
+    }
+}
